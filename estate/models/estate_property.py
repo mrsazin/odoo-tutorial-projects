a87d23f40a2debb918c969d33f9047fbe29938e0
mrsazin <azar.nazari@gmail.com>
2025-10-13 13:44:03 +0330

    Add new model:estate.property

---
+++ estate/models/estate_property.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class EstateProperty(models.Model):
+    """ This model represents estate.property."""
+    _name = 'estate.property'
+    _description = 'EstateProperty'
+
+
