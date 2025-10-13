cab6292bb3043edc9e57a48182e000a58fd564d4
mrsazin <azar.nazari@gmail.com>
2025-10-13 12:13:51 +0330

    Add new module: estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'RealState',
+    'version': '1.0',
+    'sequence': 15,
+    'summary': 'Track leads and close opportunities',
+    'description': "",
+
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
