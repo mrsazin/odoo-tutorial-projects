ebd80584556b9647a3700f843f73ae0a63e98b12
mrsazin <azar.nazari@gmail.com>
2025-10-13 14:06:21 +0330

    Add basic fields to the Real Estate Property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
+from odoo.release import description
 
 
 class EstateProperty(models.Model):
@@ -7,4 +8,21 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'EstateProperty'
 
-
+    name = fields.Char(string='Customer Name', required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='')
+    date_availability = fields.Date(string='')
+    expected_price = fields.Float(string='', digits=(16, 2))
+    selling_price = fields.Float(string='', digits=(16, 2))
+    bedrooms = fields.Integer(string='')
+    living_area = fields.Integer(string='')
+    facades = fields.Integer(string='')
+    garage = fields.Boolean(string='Active')
+    garden = fields.Boolean(string='Active')
+    garden_area = fields.Integer(string='')
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West'),
+    ], string='Status', default='draft')
