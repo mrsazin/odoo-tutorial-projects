49cd521f706530c551ea42a9999468564009fdcc
mrsazin <azar.nazari@gmail.com>
2025-10-26 18:51:57 +0330

    Using markup to display html

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,11 +1,18 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import {Counter} from "./counter/counter";
 import {Card} from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static components= {Counter, Card};
+    setup() {
+         // This will be ESCAPED (shown as text)
+         this.escapedContent = "<div class='text-primary'>escaped content</div>";
+
+         // This will be RENDERED (shown as blue HTML)
+         this.renderedContent = markup("<div class='text-primary'>rendered content</div>");
+}
 
 }
