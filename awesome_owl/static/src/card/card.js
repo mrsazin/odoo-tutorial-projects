cd6d42f49c18b104d7fc6dfdf4209062ec25d326
mrsazin <azar.nazari@gmail.com>
2025-10-28 15:31:49 +0330

    Minimizing card content

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,6 +1,14 @@
 /** @odoo-module **/
-import { Component } from "@odoo/owl";
+import { Component ,useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
+     setup() {
+        // State to track open/closed state
+        this.state = useState({ isOpen: true });
+    }
+
+    toggle() {
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
