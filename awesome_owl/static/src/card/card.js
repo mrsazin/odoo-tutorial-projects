cbe450b61fc62e178729e8a2072e67e9c740d152
mrsazin <azar.nazari@gmail.com>
2025-10-28 15:21:32 +0330

    Generic Card with slots

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -3,8 +3,4 @@ import { Component } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
-    static props = {
-        title: String,
-        content: String,
-    };
 }
